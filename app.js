aba3cd83378c20be0d2c90e6bcf2f0357c0b9ea7
v1mnt <54478007+v1mnt@users.noreply.github.com>
2022-03-15 23:04:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 function ConverterDolar() {
-    var real = parseFloat(document.getElementById("valor").value);
-    var valorConvertido = real / 5.05;
+    const real = parseFloat(document.getElementById("valor").value);
+    const valorConvertido = real / 5.05;
     document.getElementById("valorConvertido").innerHTML =
       real + " reais em dólares são = U$" + valorConvertido.toFixed(2);
   }
   
   function ConverterEuro() {
-    var euro = parseFloat(document.getElementById("valor").value);
-    var valorConvertido = euro / 5.52;
+    const euro = parseFloat(document.getElementById("valor").value);
+    const valorConvertido = euro / 5.52;
     document.getElementById("valorConvertido").innerHTML =
       euro + " reais em euros são = €" + valorConvertido.toFixed(2);
   }
   
   function ConverterLibra() {
-    var libra = parseFloat(document.getElementById("valor").value);
-    var valorConvertido = libra / 6.63;
+    const libra = parseFloat(document.getElementById("valor").value);
+    const valorConvertido = libra / 6.63;
     document.getElementById("valorConvertido").innerHTML =
       libra + " reais em libras são = £" + valorConvertido.toFixed(2);
   }
